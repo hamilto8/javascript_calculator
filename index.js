07ea4a196b26e185d81dca7c79f796f7d0209655
Michael Hamilton <michael.ts.hamilton@gmail.com>
2019-09-04 18:18:15 -0500

    Set up basic CSS, HTML and JavaScript.

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const display = document.querySelector(".display");
+let num = 4*6;
+
+const seven = document.querySelector(".seven");
+
+seven.addEventListener('click', ()=>{
+    display.textContent = 7;
+});
